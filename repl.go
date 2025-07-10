e2678a4dd3bc0c1b2481b882234fc8aa6c8cbc2b
romas <r2udis@gmail.com>
2025-07-11 01:51:42 +0300

    add repl input loop

--- repl.go
+++ repl.go
@@ -1,8 +1,26 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Fprint(os.Stderr, "Pokedex > ")
+		scanner.Scan()
+		rawInput := scanner.Text()
+		sliceInput := cleanInput(rawInput)
+		if len(sliceInput) == 0 {
+			continue
+		}
+		fmt.Println("Your command was:", sliceInput[0])
+	}
+}
 
 func cleanInput(text string) []string {
 	textLowered := strings.ToLower(text)
